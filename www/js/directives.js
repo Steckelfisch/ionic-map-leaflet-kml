dc19ca97a0901e174019a2a09fffbf81f706ab57
Dominique Delor <dominique@lehub-agence.com>
2014-11-28 10:22:31 +0100

    Add project

---
+++ www/js/directives.js
@@ -0,0 +1,32 @@
+angular.module('starter.directives', [])
+
+.directive('map', function() {
+  return {
+    restrict: 'E',
+    scope: {
+      onCreate: '&'
+    },
+    link: function ($scope, $element, $attr) {
+
+      function initialize() {
+
+        var map = new L.Map('map', {center: new L.LatLng(58.4, 43.0), zoom: 11});
+        var osm = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+        var track = new L.KML("data/KML_Samples.kml", {async: true});
+
+        track.on("loaded", function(e) { map.fitBounds(e.target.getBounds()); });
+
+        map.addLayer(track);
+        map.addLayer(osm);
+        map.addControl(new L.Control.Layers({}, {'Track':track}));
+
+      }
+
+      if (document.readyState === "complete") {
+        initialize();
+      } else {
+        alert('Erreur de chargement de la carte.');
+      }
+    }
+  }
+});
